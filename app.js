fe3caacf56f90c9593d39424ded612065b04a3b4
Duncan McLean <duncan.mclean@fiserv.com>
2023-11-30 11:01:20 -0500

    Changes made

--- app.js
+++ app.js
@@ -1,29 +1,82 @@
-// add task functionality
-function addTask() {
-    // get the input field and task list elements
-    let taskInput = document.getElementById("taskInput");
-    let taskList = document.getElementById("taskList");
+let taskInput = document.getElementById("new-task");
+let addButton = document.getElementById("addButton");
+let incompleteTasks = document.getElementById("incomplete-tasks");
+let completedTasks = document.getElementById("completed-tasks");
+let clearButton = document.getElementById("clear");
+let createNewTask = function (taskName) {
+  let listItem = document.createElement("li");
+  let checkBox = document.createElement("input");
+  let label = document.createElement("label");
+  let editInput = document.createElement("input");
+  let editButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
 
-    // console.log('taskInputs value: ', taskInput.textContent);
-    
-    // check if input field is not empty or contains only whitespace
-    if (taskInput.value.trim() != "") {
-        // create a new list item
-        let li = document.createElement("li");
+  checkBox.type = "checkBox";
+  editInput.type = "text";
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+  label.innerText = taskName;
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
 
-        // add the task text to the list item
-        li.appendChild(document.createTextNode(taskInput.value));
-        
-        // display the list item to the task list
-        taskList.appendChild(li);
+  return listItem;
+};
+let addTask = function () {
+  if (taskInput.value == "") {
+    alert("Task to be added should not be empty!");
+    return;
+  }
+  let listItem = createNewTask(taskInput.value);
+  incompleteTasks.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+  taskInput.value = "";
+};
 
-        // clear the input field
-        taskInput.value = "";
-    }
-}
+let editTask = function () {
+  let listItem = this.parentNode;
+  let editInput = listItem.querySelector("input[type=text]");
+  let label = listItem.querySelector("label");
+  let containsClass = listItem.classList.contains("editMode");
+  if (containsClass) {
+    label.innerText = editInput.value;
+  } else {
+    editInput.value = label.innerText;
+  }
+  listItem.classList.toggle("editMode");
+};
+let deleteTask = function () {
+  let listItem = this.parentNode;
+  let ul = listItem.parentNode;
+  ul.removeChild(listItem);
+};
+let taskCompleted = function () {
+  let listItem = this.parentNode;
+  completedTasks.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+};
 
+let taskIncomplete = function () {
+  let listItem = this.parentNode;
+  incompleteTasks.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+};
+addButton.addEventListener("click", addTask);
+let bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+  let checkBox = taskListItem.querySelector('input[type="checkbox"]');
+  let editButton = taskListItem.querySelector("button.edit");
+  let deleteButton = taskListItem.querySelector("button.delete");
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
+  checkBox.onchange = checkBoxEventHandler;
+};
 
-
-
-
-// delete task functionality
+let clear = function () {
+  incompleteTasks.innerHTML = "";
+  completedTasks.innerHTML = "";
+};
+clearButton.addEventListener("click", clear);
